ee48ba3fbeee892a41a284f04c747508b131a5f6
bjray <bjandraney@mac.com>
2014-07-19 13:36:33 -0500

    Add sitter object and update sitter protocol

---
+++ SitCircles/SCSitter.h
@@ -0,0 +1,13 @@
+//
+//  SCSitter.h
+//  SitCircles
+//
+//  Created by B.J. Ray on 7/19/14.
+//  Copyright (c) 2014 109Software. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SCSitter : NSObject
+
+@end
